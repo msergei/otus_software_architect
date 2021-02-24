dc2165a61022847253ec17573046d80358092cc3
Sergey Mironov <smironov@iponweb.net>
2021-02-24 16:32:33 +0300

    upd repo-upd

--- project/repo-upd.sh
+++ project/repo-upd.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
 git pull
 
 git submodule sync --recursive
+git submodule update --init --recursive --remote
+git pull --recurse-submodules
+git submodule foreach git checkout master
 git submodule foreach git pull origin master
-
